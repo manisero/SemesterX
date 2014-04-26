d4a20c440f8892bec789695e62d06e1cdd4c0d00
Jakub Turek <jkbturek@gmail.com>
2014-04-26 21:36:00 +0200

    Introduced project structure

---
+++ SPDB/project/data-access/src/main/java/pl/edu/pw/elka/model/Location.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.model;
+
+public class Location {
+    private Coordinates coordinates;
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+}
