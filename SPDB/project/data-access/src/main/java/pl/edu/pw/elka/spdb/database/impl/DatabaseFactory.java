4f57a2055efc442efb21b4a14468f7ae5b1c82f1
Jakub Turek <jkbturek@gmail.com>
2014-04-26 22:35:12 +0200

    Implemented creating database

--- SPDB/project/data-access/src/main/java/pl/edu/pw/elka/spdb/database/impl/DatabaseFactory.java
+++ SPDB/project/data-access/src/main/java/pl/edu/pw/elka/spdb/database/impl/DatabaseFactory.java
@@ -1,16 +1,26 @@
 package pl.edu.pw.elka.spdb.database.impl;
 
+import com.google.inject.Inject;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import pl.edu.pw.elka.spdb.configuration.IConfigurationProvider;
 import pl.edu.pw.elka.spdb.database.IDatabaseFactory;
 
 public class DatabaseFactory implements IDatabaseFactory {
+    private final IConfigurationProvider configurationProvider;
+
+    @Inject
+    public DatabaseFactory(IConfigurationProvider configurationProvider) {
+        this.configurationProvider = configurationProvider;
+    }
+
     @Override
     public GraphDatabaseService createDatabase() {
-        return null;
+        return createDatabase(configurationProvider.getDatabasePath());
     }
 
     @Override
     public GraphDatabaseService createDatabase(String databasePath) {
-        return null;
+        return new GraphDatabaseFactory().newEmbeddedDatabase(databasePath);
     }
 }
--- SPDB/project/data-access/src/test/java/pl/edu/pw/elka/spdb/tests/database/DatabaseFactoryTests.java
+++ SPDB/project/data-access/src/test/java/pl/edu/pw/elka/spdb/tests/database/DatabaseFactoryTests.java
@@ -3,17 +3,28 @@ package pl.edu.pw.elka.spdb.tests.database;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import pl.edu.pw.elka.spdb.configuration.IConfigurationProvider;
+import pl.edu.pw.elka.spdb.configuration.impl.ConfigurationProvider;
 import pl.edu.pw.elka.spdb.database.IDatabaseFactory;
 import pl.edu.pw.elka.spdb.database.impl.DatabaseFactory;
 
+import java.io.File;
+
 public class DatabaseFactoryTests extends TestCase {
     @Test
-    public void createDatabaseTest() {
-        IDatabaseFactory databaseFactory = new DatabaseFactory();
+    public void testCreateDatabaseMethod() {
+        IConfigurationProvider configurationProvider = new ConfigurationProvider();
+        IDatabaseFactory databaseFactory = new DatabaseFactory(configurationProvider);
 
         GraphDatabaseService service = databaseFactory.createDatabase("target/test.db");
         boolean serviceIsOpen = service != null;
         service.shutdown();
+
+        File file = new File("target/test.db");
+
+        if (file.exists()) {
+            file.delete();
+        }
 
         assertTrue(serviceIsOpen);
     }
