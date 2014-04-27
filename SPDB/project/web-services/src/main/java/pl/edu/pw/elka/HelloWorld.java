a4b16416799439265f7e6c2e486a18c4232d67c1
Jakub Turek <jkbturek@gmail.com>
2014-04-27 23:20:15 +0200

    Polished pom files

--- SPDB/project/web-services/src/main/java/pl/edu/pw/elka/HelloWorld.java
+++ SPDB/project/web-services/src/main/java/pl/edu/pw/elka/HelloWorld.java
@@ -1,10 +1,6 @@
 package pl.edu.pw.elka;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
--- SPDB/project/web-services/src/test/java/pl/edu/pw/elka/HelloWorldIT.java
+++ SPDB/project/web-services/src/test/java/pl/edu/pw/elka/HelloWorldIT.java
@@ -1,19 +1,18 @@
 package pl.edu.pw.elka;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.core.Response;
-
 import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.MappingJsonFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelloWorldIT {
     private static String endpointUrl;
