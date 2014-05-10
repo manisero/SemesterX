4499170ae9ba9562a1a966fc49a2597cb85db85f
unknown <michalaniserowicz@gmail.com>
2014-05-10 13:29:31 +0200

    Created Board class

--- SPOP/projekt/dev/board.hs
+++ SPOP/projekt/dev/board.hs
@@ -1,17 +1,20 @@
 module Board where
 
+-- generic classes
+class Board tBoard where
+	fields :: tBoard -> [[Field]]
+	size :: tBoard -> Int
+	size board = length (fields board)
+	field :: tBoard -> Int -> Int -> Field
+	field board x y = ((fields board) !! y) !! x
+	moves :: Board tBoard => tBoard -> Player -> [Move]
+
+-- tic-tac-toe types
 data Player = Crosses | Circles
-
 data Field = Empty | Cross | Circle deriving (Eq, Show)
-data Board = Board { fields :: [[Field]] }
+type Move = (Int, Int)
 
-type Movement = (Int, Int)
-
-size :: Board -> Int
-size board = length (fields board)
-
-field :: Board -> Int -> Int -> Field
-field board x y = ((fields board) !! y) !! x
-
-moves :: Board -> Player -> [Movement]
-moves board player = [(x, y) | x <- [1 .. (size board)], y <- [1 .. (size board)], field board x y == Empty]
+data TTTBoard = TTTBoard [[Field]]
+instance Board TTTBoard where
+	fields (TTTBoard fs) = fs
+	moves board player = [(x, y) | x <- [0 .. ((size board) - 1)], y <- [0 .. ((size board) - 1)], field board x y == Empty]
--- SPOP/projekt/dev/main.hs
+++ SPOP/projekt/dev/main.hs
@@ -2,11 +2,18 @@ module Main where
 import Board
 
 main :: IO ()
-main = putStrLn (show (fields board))
+main = putStrLn (show (moves board1 Crosses))
 
-board :: Board
-board = Board [
-				[Empty, Empty, Empty],
-				[Empty, Empty, Empty],
-				[Empty, Empty, Empty]
-			]
+board :: TTTBoard
+board = TTTBoard [
+					[Empty, Empty, Empty],
+					[Empty, Empty, Empty],
+					[Empty, Empty, Empty]
+				]
+
+board1 :: TTTBoard
+board1 = TTTBoard [
+					[Empty, Circle, Circle],
+					[Cross, Empty, Circle],
+					[Cross, Cross, Empty]
+				]
