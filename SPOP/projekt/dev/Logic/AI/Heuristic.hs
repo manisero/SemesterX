bbf8d34b977dd6ce778ea675fd2be20418762b7b
unknown <michalaniserowicz@gmail.com>
2014-05-13 19:50:43 +0200

    Code cleanup

--- SPOP/projekt/dev/Logic/AI/Heuristic.hs
+++ SPOP/projekt/dev/Logic/AI/Heuristic.hs
@@ -11,21 +11,21 @@ alphaBetaHeuristicDepth = 8
 
 
 
--- getChildren function
-getChildren :: Board -> Player -> [Board]
-getChildren board player = map (\move -> applyMove move board) (getMoves board player)
-
-
-
 -- aiMove function
 aiMove :: Board -> Player -> Board
 aiMove currentBoard player = pickChild (getChildren currentBoard player) player alphaBetaHeuristicDepth
 
 pickChild :: [Board] -> Player -> Int -> Board
-pickChild [move] _ _ = move
-pickChild (move1:move2:moves) player depth = if (alphaBetaHeuristic move1 (getPlayerOpponent player) player depth >= alphaBetaHeuristic move2 (getPlayerOpponent player) player depth)
-											 then pickChild (move1:moves) player depth
-											 else pickChild (move2:moves) player depth
+pickChild [child] _ _ = child
+pickChild (child1:child2:children) player depth = if (alphaBetaHeuristic child1 (getPlayerOpponent player) player depth >= alphaBetaHeuristic child2 (getPlayerOpponent player) player depth)
+												  then pickChild (child1:children) player depth
+												  else pickChild (child2:children) player depth
+
+
+
+-- getChildren function
+getChildren :: Board -> Player -> [Board]
+getChildren board player = map (\move -> applyMove move board) (getMoves board player)
 
 
 
@@ -34,24 +34,24 @@ alphaBetaHeuristic :: Board -> Player -> Player -> Int -> Int
 alphaBetaHeuristic board currentPlayer rootPlayer depth = alphaBetaHeuristic' board currentPlayer rootPlayer (minBound::Int) (maxBound::Int) depth
 
 alphaBetaHeuristic' :: Board -> Player -> Player -> Int -> Int -> Int -> Int
-alphaBetaHeuristic' board currentPlayer rootPlayer alpha beta depth = if (depth == 0 || length moves == 0)
+alphaBetaHeuristic' board currentPlayer rootPlayer alpha beta depth = if (depth == 0 || getResult board rootPlayer /= Unsettled)
 																	  then Logic.Game_WolfNSheep.getScore board rootPlayer
 																	  else if (currentPlayer == rootPlayer)
-																		then alphaLoop moves currentPlayer rootPlayer alpha beta depth
-																		else betaLoop moves currentPlayer rootPlayer alpha beta depth
+																		then alphaLoop children currentPlayer rootPlayer alpha beta depth
+																		else betaLoop children currentPlayer rootPlayer alpha beta depth
 																		where
-																			moves = getChildren board currentPlayer
+																			children = getChildren board currentPlayer
 
 alphaLoop :: [Board] -> Player -> Player -> Int -> Int -> Int -> Int
 alphaLoop [] _ _ alpha _ _ = alpha
-alphaLoop (move:moves) currentPlayer rootPlayer alpha beta depth = if (newAlpha >= beta)
-																   then newAlpha
-																   else alphaLoop moves currentPlayer rootPlayer newAlpha beta depth
-																	where newAlpha = max alpha (alphaBetaHeuristic' move (getPlayerOpponent currentPlayer) rootPlayer alpha beta (depth - 1))
+alphaLoop (child:children) currentPlayer rootPlayer alpha beta depth = if (newAlpha >= beta)
+																	   then newAlpha
+																	   else alphaLoop children currentPlayer rootPlayer newAlpha beta depth
+																		where newAlpha = max alpha (alphaBetaHeuristic' child (getPlayerOpponent currentPlayer) rootPlayer alpha beta (depth - 1))
 
 betaLoop :: [Board] -> Player -> Player -> Int -> Int -> Int -> Int
 betaLoop [] _ _ _ beta _ = beta
-betaLoop (move:moves) currentPlayer rootPlayer alpha beta depth = if (newBeta <= alpha)
-																  then newBeta
-																  else betaLoop moves currentPlayer rootPlayer alpha newBeta depth
-																	where newBeta = min beta (alphaBetaHeuristic' move (getPlayerOpponent currentPlayer) rootPlayer alpha beta (depth - 1))
+betaLoop (child:children) currentPlayer rootPlayer alpha beta depth = if (newBeta <= alpha)
+																	  then newBeta
+																	  else betaLoop children currentPlayer rootPlayer alpha newBeta depth
+																		where newBeta = min beta (alphaBetaHeuristic' child (getPlayerOpponent currentPlayer) rootPlayer alpha beta (depth - 1))
