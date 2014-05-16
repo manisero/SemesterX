d883a6a4fa138598fd5972d848fc4545af35d514
unknown <michalaniserowicz@gmail.com>
2014-05-16 20:38:36 +0200

    Implemented handling read/write exceptions

--- SPOP/projekt/dev/Presentation/SaveLoad.hs
+++ SPOP/projekt/dev/Presentation/SaveLoad.hs
@@ -5,6 +5,7 @@ module Presentation.SaveLoad(
 
 import System.IO.Error
 import Control.Exception
+import Data.Maybe
 import Logic.Game
 
 -- saveGame function
@@ -21,14 +22,6 @@ saveGame board = do
 						  handleSaveError
 
 
--- handleSaveError function
-handleSaveError :: IOError -> IO Bool
-handleSaveError e = do
-						putStrLn ""
-						putStrLn ("Error: " ++ ioeGetErrorString e)
-						return False
-
-
 
 -- loadGame function
 loadGame :: IO (Maybe Board)
@@ -36,12 +29,40 @@ loadGame = do
 			putStrLn ""
 			putStrLn "Type file path:"
 			path <- getLine
-			content <- readFile path
-			putStrLn ""
-			putStrLn ("Game loaded from: " ++ path)
-			return (readBoard content)
+			content <- catch (do
+								fileContent <- readFile path
+								return (Just fileContent))
+							 handleLoadError
+			if (isJust content)
+				then do
+					let board = readBoard (fromJust content)
+					if (isJust board)
+						then do
+							putStrLn ""
+							putStrLn ("Game loaded from: " ++ path)
+							return board
+						else handleError "Invalid file content" Nothing
+				else return Nothing
+
 
 readBoard :: String -> Maybe Board
 readBoard string = case reads string of
 					[(board, "")] -> Just board
 					_         -> Nothing
+
+
+
+-- handleError function
+handleError :: String -> tResult -> IO tResult
+handleError errorMessage result = do
+								putStrLn ""
+								putStrLn ("Error: " ++ errorMessage)
+								return result
+
+
+handleSaveError :: IOError -> IO Bool
+handleSaveError e = handleError (ioeGetErrorString e) False
+
+
+handleLoadError :: IOError -> IO (Maybe string)
+handleLoadError e = handleError (ioeGetErrorString e) Nothing
